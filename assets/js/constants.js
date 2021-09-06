efc2ec5b9daea71c72da342f03c0f0fa153374a3
Anjelika86 <fominaangelika1986@gmail.com>
2021-09-06 16:49:20 +0300

    Add constants.js

---
+++ assets/js/constants.js
@@ -0,0 +1,6 @@
+const SUPPORTED_SOCIAL_NETWORKS = new Map([
+  ["www.instagram.com", ["fa", "fa-instagram", "social"]],
+  ["twitter.com", ["fa", "fa-twitter", "social"]],
+  ["www.facebook.com", ["fa", "fa-facebook", "social"]],
+]);
+// const url = new URL('https://www.facebook.com/JasonStatham/')
