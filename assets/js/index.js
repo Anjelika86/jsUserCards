e8bcdc41e9caedf84d75379f58d0ce88ea5315f8
Anjelika <fominaangelika86@gmail.com>
2021-09-08 08:43:03 +0300

    Fix index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,43 +7,6 @@ cardsContainer.append(...userCards);
 
 function generateUserCard(userObj) {
   const { id, firstName, lastName, profilePicture, contacts } = userObj;
-  const img = createElement("img", {
-    classNames: ["img"],
-    attrs: { src: profilePicture, alt: firstName, "data-id": id },
-  });
-
-  img.addEventListener("error", deleteHandler);
-  img.addEventListener("load", imageLoadeHandler);
-
-  const initails = createElement(
-    "div",
-    { classNames: ["initails"] },
-    document.createTextNode(
-      firstName
-        .trim()
-        .split(" ")
-        .map((word) => word[0])
-        .join(" ")
-    ),
-    document.createTextNode(
-      lastName
-        .trim()
-        .split(" ")
-        .map((word) => word[0])
-        .join(" ")
-    )
-  );
-
-  initails.style.backgroundColor = stringToColour(firstName.trim());
-
-  const imgWrapper = createElement(
-    "div",
-    {
-      classNames: ["imgWrapper"],
-      attrs: { id: `wrapper${id}` },
-    },
-    initails
-  );
 
   const userName = createElement(
     "h2",
@@ -69,7 +32,7 @@ function generateUserCard(userObj) {
   const article = createElement(
     "article",
     { classNames: ["userCard"] },
-    imgWrapper,
+    createImageWrapper(userObj),
     userName,
     cardDescription,
     linkWrapper
