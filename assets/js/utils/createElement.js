c02260dd62f8d1cafa68056f9c31ea41b89fde2d
Anjelika <fominaangelika86@gmail.com>
2021-09-10 10:46:26 +0300

    Add option listener

--- assets/js/utils/createElement.js
+++ assets/js/utils/createElement.js
@@ -1,11 +1,22 @@
 function createElement(tagName, options, ...children) {
-  const { classNames = [], attrs = {}, onClick = () => {} } = options;
+  const {
+    classNames = [],
+    attrs = {},
+    listener = {},
+    onClick = () => {},
+  } = options;
   const element = document.createElement(tagName);
   element.classList.add(...classNames);
+
   const attributesTuples = Object.entries(attrs);
   for (const [key, value] of attributesTuples) {
     element.setAttribute(key, value);
   }
+  const listenerTuples = Object.entries(listener);
+  for (const [key, value] of listenerTuples) {
+    element.addEventListener(key, value);
+  }
+
   element.onClick = onClick;
   element.append(...children);
   return element;
